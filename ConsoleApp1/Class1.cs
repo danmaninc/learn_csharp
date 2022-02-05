3db6a3158e38c1568739c8b776b7807cbad3ef29
Dan <whatdanman@gmail.com>
2022-02-05 10:32:13 +0300

    Создание класс Копейщика — Spearman

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Spearman
+    {
+        private int attack;
+        private int defense;
+        private int damage;
+        private int health;
+        private int speed;
+        private int price;
+    }
+}
