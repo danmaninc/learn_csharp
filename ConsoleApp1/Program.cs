d0e5134456cd0ae8b95ab9a96c03806076c19a23
Dan <whatdanman@gmail.com>
2022-02-05 11:44:19 +0300

    Исправлено «ИЛИ» на «И»

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
             int attackSpearman1 = spearman1.Storming();
             int attackSpearman2 = spearman2.Storming();
 
-            while (!spearman1.isDead() || !spearman2.isDead())
+            while (!spearman1.isDead() && !spearman2.isDead())
             {
                 spearman1.getHit(attackSpearman2);
                 if (spearman1.isDead())
